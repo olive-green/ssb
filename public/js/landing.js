efbe6935031c762b8139e0c117bab16c5872f0ac
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-05-20 13:35:22 +0530

    Updated css files

--- public/js/landing.js
+++ public/js/landing.js
@@ -10,4 +10,34 @@ setInterval(() => {
 setInterval(() => {
     first.style.display = "block";
     second.style.display = "none";
-},4000)
+}, 4000)
+
+
+/* Blog Section */
+/* Fetch url = https://localhost:3000/articles/getBlogs */
+/* @desc: Fetching data through api call to backend blog site */
+let length = 0;
+async function getBlog() {
+    const res = await fetch("http://localhost:5000/articles/getBlogs")
+    const data = await res.json()
+    console.log(data)
+    length = data.data.length;
+    console.log(length)
+    /* Card Section */
+let card = document.querySelectorAll(".card");
+let cardTitle = document.querySelectorAll(".title");
+const cardDesc = document.querySelectorAll(".copy");
+
+    for (let i = 0; i < length; i++){
+        console.log(data.data[i].title);
+        cardTitle[i].innerHTML = data.data[i].title;
+        cardDesc[i].innerHTML = data.data[i].Shortdescription;
+    }
+}
+
+
+getBlog();
+
+
+
+
